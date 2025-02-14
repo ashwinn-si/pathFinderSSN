073da04a360b9478f947a3280e62efe9df8cf8a6
ashwin_si <ashwin23cse@gmail.com>
2025-02-14 10:47:36 +0530

    signup controller bug resolved

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        displayStar: {
+          '0%': { transform: 'rotateX(100deg) rotateY(100deg) translateY(10px)' },
+          '100%': { transform: 'rotateX(0deg) rotateY(0deg) translateY(0px)' }
+        },
+        checkStar: {
+          '0%': { transform: 'rotate(0deg)' },
+          '20%': { transform: 'rotate(-20deg)' },
+          '50%': { transform: 'rotate(20deg)' },
+          '80%': { transform: 'rotate(-20deg)' },
+          '100%': { transform: 'rotate(0deg)' }
+        }
+      }
+    }
   },
   plugins: [
     require('daisyui'),
